33a139c4947d3fb6b76f29e4646091fda44f6a63
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-06-08 20:41:28 +0500

    1

---
+++ 20220608.cpp
@@ -0,0 +1,39 @@
+﻿/*
+есть динамический массив, сами определяете чем он заполнен. 
+Ваша задача при помощи умного указателя, просто напечатать все числа,
+которые делятся и на 5 и на 3
+*/
+
+#include <iostream>
+using namespace std;
+
+void createArr(shared_ptr<int[]> &p, int size) {
+	for (int i = 0; i < size; i++) {
+		p[i] = rand() % 100 + 1;
+	}
+}
+
+inline bool divValue(int div , int firstDive, int secondDiv) {
+	if ((div % firstDive == 0) && (div % secondDiv == 0)) {
+		return 1;
+	}
+	return 0;
+}
+
+int main(){
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+
+	int const SIZE = 1000;
+
+	shared_ptr<int[]> ptr(new int[SIZE] {0});
+	createArr( ptr, SIZE);
+
+	for (int i = 0; i < SIZE; i++) {
+		if (divValue(ptr[i], 3, 5)) {
+			cout << ptr[i] << endl;
+		}
+	}
+
+	return 0;
+}
